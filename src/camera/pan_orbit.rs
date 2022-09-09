9fd6be4840beb50b8173812b41e538c8e5050acb
parnikasaxena <paxsethorld@gmail.com>
2022-09-09 14:48:51 -0400

    works!

--- src/camera/pan_orbit.rs
+++ src/camera/pan_orbit.rs
@@ -129,7 +129,7 @@ pub fn spawn_camera(mut commands: Commands) {
     let radius = translation.length();
 
     commands
-        .spawn_bundle(PerspectiveCameraBundle {
+        .spawn_bundle(Camera3dBundle {
             transform: Transform::from_translation(translation).looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         })
